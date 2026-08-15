2fc55a7111f5aed961b9af8c803dcbdd9ca2907d
jonahjayasingh <jayasinghjonah24@gmail.com>
2026-08-15 21:30:09 +0530

    add

---
+++ springbootorm/src/test/java/com/example/springbootorm/SpringbootormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
